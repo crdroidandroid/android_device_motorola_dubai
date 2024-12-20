8ce0f8b471ab1764a579395f8b1a6711dc436e20
Rakhshan Ali <alirakhshan7070@gmail.com>
2024-12-21 04:47:28 +0530

    dubai: Enable Blur support

--- lineage_dubai.mk
+++ lineage_dubai.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from dubai device
 $(call inherit-product, device/motorola/dubai/device.mk)
 
+# disable/enable blur support, default is false
+TARGET_ENABLE_BLUR := true
+
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
